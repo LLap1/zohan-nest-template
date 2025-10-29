227ece0d5f734385d8d95b7adb3e9239bc356da9
Yonatan <yonatan@Mayas-MacBook-Pro.local>
2025-10-29 13:01:40 +0200

    added swagger and orpc playground

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { name, description, version } from '../package.json';
-
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import packageJson from '../package.json';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  })
   const config = new DocumentBuilder()
-    .setTitle(name)
-    .setDescription(description)
-    .setVersion(version)
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('swagger', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+bootstrap()
